599f5a012ab28ec07d4399c66e026212f96e8b4e
trufanova <5014685@mail.ru>
2021-01-11 15:32:18 +0300

    add .gitignore

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello, Hexlet!');
+
+// По общепринятому соглашению, lodash импортируется под именем _
+import _ from "lodash";
+
+// Пересечение массивов
+_.intersection([2, 1], [2, 3]); // => [2]
+
+// capitalize делает первую букву заглавной
+_.capitalize("hello"); // Hello
+console.log(_.last(['one', 'two']));
+
